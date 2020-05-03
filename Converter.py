2a0fb9766b0ab3ebe811005ceb6d70b9e4942f09
jumbocarrot0 <46736855+jumbocarrot0@users.noreply.github.com>
2020-05-03 13:46:16 +1000

    Update

--- Converter.py
+++ Converter.py
@@ -1,20 +1,12 @@
+#!/usr/bin/env python
 from tkinter import Tk
 from tkinter.filedialog import askopenfilenames
 import os 
 import zipfile
 from shutil import make_archive
 from pathlib import Path
-
-dataSave = open("Program Source/Data.txt", "r")
-saveRead = dataSave.readlines()
-dataSave.close()
-
-if 'True' in saveRead[0]:
-	AskAgain = True
-else:
-	AskAgain = False
+import xml.etree.ElementTree as ET
 	
-
 Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
 modFiles = list(askopenfilenames()) # show an "Open" dialog box and return the path to the selected file
 print(str(modFiles))
@@ -44,22 +36,16 @@ for mods in range(0, len(modFiles)):
 		
 	print('converting ' + str(modname))
 		
-	blueprintsData = []
+	blueprintFiles = []
 		
 	if os.path.exists("Mod Source/data/blueprints.xml.append") is True:
-		xml = open("Mod Source/data/blueprints.xml.append", "r")
-		blueprintsData += xml.readlines()
-		xml.close()
+		blueprintFiles.append("Mod Source/data/blueprints.xml.append")
 		
 	if os.path.exists("Mod Source/data/dlcBlueprints.xml.append") is True:
-		xml = open("Mod Source/data/dlcBlueprints.xml.append", "r")
-		blueprintsData += xml.readlines()
-		xml.close()
+		blueprintFiles.append("Mod Source/data/dlcBlueprints.xml.append")
 		
 	if os.path.exists("Mod Source/data/dlcBlueprintsOverwrite.xml.append") is True:
-		xml = open("Mod Source/data/dlcBlueprintsOverwrite.xml.append", "r")
-		blueprintsData += xml.readlines()
-		xml.close()
+		blueprintFiles.append("Mod Source/data/dlcBlueprintsOverwrite.xml.append")
 	
 	
 	playerShipOrder = ['PLAYER_SHIP_HARD', 'PLAYER_SHIP_CIRCLE', 'PLAYER_SHIP_FED', 'PLAYER_SHIP_ENERGY', 'PLAYER_SHIP_MANTIS', 'PLAYER_SHIP_JELLY', 'PLAYER_SHIP_ROCK', 'PLAYER_SHIP_STEALTH', 'PLAYER_SHIP_CRYSTAL', 'PLAYER_SHIP_ANAEROBIC', 'PLAYER_SHIP_HARD_2', 'PLAYER_SHIP_CIRCLE_2', 'PLAYER_SHIP_FED_2', 'PLAYER_SHIP_ENERGY_2', 'PLAYER_SHIP_MANTIS_2', 'PLAYER_SHIP_JELLY_2', 'PLAYER_SHIP_ROCK_2', 'PLAYER_SHIP_STEALTH_2', 'PLAYER_SHIP_CRYSTAL_2', 'PLAYER_SHIP_ANAEROBIC_2', 'PLAYER_SHIP_HARD_3', 'PLAYER_SHIP_CIRCLE_3', 'PLAYER_SHIP_FED_3', 'PLAYER_SHIP_ENERGY_3', 'PLAYER_SHIP_MANTIS_3', 'PLAYER_SHIP_JELLY_3', 'PLAYER_SHIP_ROCK_3', 'PLAYER_SHIP_STEALTH_3']
@@ -71,58 +57,42 @@ for mods in range(0, len(modFiles)):
 	modBasePlayerShipNames = []
 	#nameCheck = False
 	
-	for lines in range(0, len(blueprintsData)):
-		if '<shipBlueprint name="' in blueprintsData[lines]:
-			for x in range(0, len(playerShipOrder)):
-				if '"' + playerShipOrder[x] + '"' in blueprintsData[lines]:
-					modPlayerShips.append(lines)
-					modPlayerShipNames.append(playerShipOrder[x])
+	for file in blueprintFiles:
+	
+		xml = open(file, "r")
+		code = xml.readlines()
+		xml.close()
 		
-	for x in range(0, len(modPlayerShipNames)):
-		modPlayerShipNames[x] = (modname.upper() + '_MOD_HYPER_' + modPlayerShipNames[x].upper()).upper()
-		if x // 10 == 0:
-			modBasePlayerShipNames.append(modPlayerShipNames[x])
-	
-	
-	#Implementation
-	
-	for ships in range(0, len(modPlayerShips)):
-		if '<shipBlueprint name="' in blueprintsData[modPlayerShips[ships]]:
-			for x in range(0, len(playerShipOrder)):
-				if '"' + playerShipOrder[x] + '"' in blueprintsData[modPlayerShips[ships]]:
-					blueprintsData[modPlayerShips[ships]] = blueprintsData[modPlayerShips[ships]][:blueprintsData[modPlayerShips[ships]].index(playerShipOrder[x])] + modPlayerShipNames[ships] + blueprintsData[modPlayerShips[ships]][blueprintsData[modPlayerShips[ships]].index(playerShipOrder[x])+len(playerShipOrder[x]):]
+		if '<FTL>' not in code and '</FTL>' not in code:
 		
-	xml = open("Mod Source/data/blueprints.xml.append", "w")
-	for lines in blueprintsData:
-		xml.writelines(lines)
-	xml.close()	
-	
-	if AskAgain is True:
-	
-		HackingNerf = input('Include Hacking Drone Nerf (explodes on disabling) (type y/n)\n').lower()
-		if HackingNerf == ('y' or 'yes'):
-			HackingNerf = True
-		elif HackingNerf == ('n' or 'no'):
-			HackingNerf = False
-		else:
-			HackingNerf = False
-			print('Input accepted as a no')
+			declaration = ''
 		
-		AskAgain = input('Ask again? (type y/n)\n').lower()
-		print(AskAgain)
-		if AskAgain == ('y' or 'yes'):
-			print('check1')
-			AskAgain = True
-		elif AskAgain == ('n' or 'no'):
-			print('check2')
-			AskAgain = False
-		else:
-			print('check3')
-			AskAgain = False
-			print('Input accepted as a no')
-		print(AskAgain)
-	else:
-		HackingNerf = 'True' in saveRead[1]
+			for x in range(0, len(code)):
+				if '<?xml' in code[x]:
+					declaration = code[x]
+					del code[x]
+					break
+		
+			code.insert(0, '<FTL>')
+			code.insert(0, declaration)
+			code.append('</FTL>')
+			
+			xml = open(file, "w")
+			code = xml.writelines(code)
+			xml.close()
+		
+		tree = ET.parse(file)
+		root = tree.getroot()
+		
+		for child in root:
+			if child.tag == 'shipBlueprint':
+				for child2 in child:
+					if child2.tag == 'name':
+						modBasePlayerShipNames.append(child.attrib['name'] + '_' + child2.text)
+						child.set('name', modBasePlayerShipNames[-1])
+						break
+						
+		tree.write(file)
 	
 	xml = open("Mod Source/data/hyperspace.xml.append", "w")
 	xml.writelines('<ships>\n')
@@ -137,17 +107,9 @@ for mods in range(0, len(modFiles)):
 		else:
 			typeC = False
 		
-	xml.writelines('\t<ship name="' + modBasePlayerShipNames[x] + '" b="' + str(typeB).lower() + '" c="' + str(typeC).lower() + '"/>\n')
+		xml.writelines('\t<ship name="' + modBasePlayerShipNames[x] + '" b="' + str(typeB).lower() + '" c="' + str(typeC).lower() + '"/>\n')
 	xml.writelines('</ships>\n\n')
-	xml.writelines('<titleScreen enabled="false"/>\n')
-	xml.writelines('<console enabled="false"/>\n')
-	xml.writelines('<hackingDroneFix enabled="'+str(HackingNerf).lower()+'"/>\n')
 	xml.close()
-	
-	dataSave = open("Program Source/Data.txt", "w")
-	dataSave.writelines(str(AskAgain) + '\n')
-	dataSave.writelines(str(HackingNerf) + '\n')
-	dataSave.close()
 	
 	make_archive('Hyperspace ' + modname, 'zip', "Mod Source")
 			
